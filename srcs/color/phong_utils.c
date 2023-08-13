db4f0ae1a766dbd4bfb57f9fa4a14b6843f08388
NEIL-smtg <sunghui2000@hotmail.com>
2023-08-14 02:58:20 +0800

    added refraction ray

--- srcs/color/phong_utils.c
+++ srcs/color/phong_utils.c
@@ -6,7 +6,7 @@
 /*   By: suchua < suchua@student.42kl.edu.my>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 23:41:18 by suchua            #+#    #+#             */
-/*   Updated: 2023/08/10 23:52:56 by suchua           ###   ########.fr       */
+/*   Updated: 2023/08/14 02:57:17 by suchua           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,25 @@ t_vec3	reflect(t_vec3 incident, t_vec3 surface_normal)
 
 	dot = vec3_dot(incident, surface_normal);
 	ref = vec3_sub(incident, vec3_mul(2.0 * dot, surface_normal));
+	return (normalize(ref));
+}
+
+t_vec3	refract(t_vec3 incident, t_vec3 surface_normal, double n1, double n2)
+{
+	double	cos_theta1;
+	double	theta1;
+	double	sin_theta2;
+	double	theta2;
+	t_vec3	ref;
+
+	cos_theta1 = vec3_dot(incident, surface_normal);
+	theta1 = acos(cos_theta1);
+	sin_theta2 = (n1 / n2) * sin(theta1);
+	if (sin_theta2 > 1.0)
+		return (reflect(incident, surface_normal));
+	theta2 = asin(sin_theta2);
+	ref = vec3_mul(n1 / n2, incident);
+	ref = vec3_sub(ref, vec3_mul(cos(theta2), surface_normal));
 	return (normalize(ref));
 }
 
